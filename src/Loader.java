640d8817278c2149f78ea6f595b493c06e0bb6b8
rmit-s3444615-ryan-teo <s3444615@student.rmit.edu.au>
2016-09-24 22:22:50 +1000

    Made loader class

---
+++ src/Loader.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Loader {
+	
+	ArrayList<Person> loadPeople() throws FileNotFoundException{
+		FileReader fr = new FileReader("game1.config");
+		BufferedReader br = new BufferedReader(fr);
+		ArrayList<Person> people = null;
+		
+		
+		
+		return people;
+	}
+}
